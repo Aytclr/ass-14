2b7ffdfa17c8f7537f94079a233bab7fd24802e4
AYTC <aytacalir@gmail.com>
2024-09-12 21:01:59 +0300

    t2

--- src/component/Typography.jsx
+++ src/component/Typography.jsx
@@ -8,7 +8,13 @@ const Typography = () => {
         <p className="text-4xl ">Typography</p>
         <p className="text-base ">Typography</p>
         <p className="text-xs ">Typography</p>
-        <p className="text-xl ">Typography</p>
+        <p className="text-6xl ">Typography</p>
+        <p className="text-[3rem] ">Typography</p>
+
+        <article>
+           <p className='line-clamp-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus sunt, ipsa inventore sit natus officiis, veniam sapiente maxime reiciendis debitis sint quidem dolor aperiam velit amet, eius facilis. Excepturi in obcaecati, et eum eaque aut.</p> 
+        </article>
+
     </div>
   )
 }
